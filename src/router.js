54e6b936738662ca8e67b783e97f1652db97e88a
udonna-cell <orabuezestanley@gmail.com>
2023-03-22 12:35:48 -0700

    routes are set and ready

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Products from "./pages/Products/Products"
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+// import Single_product from "./pages/Single_product/Single_product"
+
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />}/>
+                <Route path="/about" element={<About />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="*" element={ErrorPage}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
